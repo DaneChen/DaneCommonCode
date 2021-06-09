40f436ce88f9554d525c697b4b52373dd30d104c
chengenrong <chengenrong@myhexin.com>
2021-06-09 17:01:50 +0800

    权限申请库简单封装

---
+++ permission/src/main/java/com/dane/permission/PermissionListener.java
@@ -0,0 +1,17 @@
+package com.dane.permission;
+
+
+public interface PermissionListener {
+
+    /**
+     * 通过授权
+     * @param permission
+     */
+    void permissionGranted(String[] permission);
+
+    /**
+     * 拒绝授权
+     * @param permission
+     */
+    void permissionDenied(String[] permission);
+}
